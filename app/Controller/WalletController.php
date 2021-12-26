9ffd40697b6ceb5e8c4ccb766b81aa3b54aaf342
Tran Thi Thu Thanh <kindaichicao@gmail.com>
2021-12-26 22:02:36 +0700

    [Add]: bankconnect feature

--- app/Controller/WalletController.php
+++ app/Controller/WalletController.php
@@ -18,17 +18,30 @@ class WalletController extends Controller
     public function wallet()
     {
         Auth::checkAuthentication();
-        //Auth::ktraquyen("CN01");
+        //Auth::ktraquyen("CN11");
         $this->View->render('wallet/wallet');
     }
     
 
     public function bankConnection(){
-
+        Auth::checkAuthentication();
+        //Auth::ktraquyen("CN11");
+        //$matknh = Request::post('matknh');
+        $makh = Cookie::get('user_email');
+        $data = WalletModel::bankConnection($makh);
+        $response = [
+            'thanhcong' => $data
+        ];
+        $this->View->renderJSON($response);
     }
 
     public function topUp(){
-        
+        $tien = Cookie::get('tien');
+        $data = WalletModel::topUp($tien);
+        $response = [
+            'thanhcong' => $data
+        ];
+        $this->View->renderJSON($response);
     }
 
     public function withdraw(){
@@ -68,22 +81,9 @@ class WalletController extends Controller
     public function checkConnection(){
         Auth::checkAuthentication();
         //Auth::ktraquyen("CN11");
-        $point = WalletModel::getPoint(Cookie::get('user_email'));
-        $hangs = WalletModel::getList();
-        foreach($hangs as $value){
-            if($point >= $value->muc_diem){
-                $rank = $value->ten_hang;
-            } else {
-                $rankNew = $value->ten_hang;
-                $diemNew = $value->muc_diem - $point;
-                break;
-            }
-        }
+        $check = WalletModel::checkConnection(Cookie::get('user_matk'));
         $response = [
-            'diem' => $point,
-            'diemmoi' => $diemNew,
-            'rank' => $rank,
-            'rankNew' => $rankNew
+            'thanhcong' => $check,
         ];
         $this->View->renderJSON($response);
     }
--- app/Model/WalletModel.php
+++ app/Model/WalletModel.php
@@ -7,17 +7,35 @@ use App\Core\DatabaseFactory;
 use PDO;
 
 class WalletModel{
-    public static function bankConnection(){
+    public static function bankConnection($makh){
 
         // Sử dụng bên thứ 3 để kết nối ngân hàng, ở đây sẽ mặc định là khách hàng điền đúng thông tin
+        $database = DatabaseFactory::getFactory()->getConnection();
+        $query = $database->prepare("SELECT * FROM khach_hang WHERE email = :matk LIMIT 1");
+        $query->execute([':matk' => $makh]);
 
-        return true;
+        $data = $query->fetch();
+        $sql = "INSERT INTO `vi_thanh_toan`(`ma_kh`, `so_du`) VALUES (".$data->ma_kh.",0)";
+        
+        $query2 = $database->query($sql);
+        $count = $query2->rowCount();
+        if ($count == 1) {
+            return true;
+        }
+        return false;
     }
 
-    public static function topUp(){
+    public static function topUp($tien){
 
         // sử dụng bên thứ 3 kiểm tra, ở đây mặc định khách hàng luôn nạp đúng số tiền trong thẻ
-        return true;
+        $database = DatabaseFactory::getFactory()->getConnection();
+        $sql = "UPDATE `vi_thanh_toan` SET so_du = ".$tien;
+        $query = $database->query($sql);
+        $count = $query->rowCount();
+        if ($count == 1) {
+            return true;
+        }
+        return false;
     }
 
     public static function withdraw(){
@@ -44,9 +62,13 @@ class WalletModel{
 
     public static function checkConnection($email){
         $database = DatabaseFactory::getFactory()->getConnection();
+        $query = $database->prepare("SELECT * FROM khach_hang WHERE ma_tk = :matk LIMIT 1");
+        $query->execute([':matk' => $email]);
 
-        $query = $database->prepare("SELECT * FROM khach_hang kh, vi_thanh_toan vi  WHERE kh.email = :email AND kh.ma_kh = vi.ma_kh LIMIT 1");
-        $query->execute([':email' => $email]);
+        $data = $query->fetch();
+
+        $query = $database->prepare("SELECT * FROM vi_thanh_toan vi  WHERE ma_kh = :ma_kh LIMIT 1");
+        $query->execute([':ma_kh' => $data->ma_kh]);
 
         if($query->fetch()){
             return true;
@@ -57,6 +79,8 @@ class WalletModel{
 
     public static function getList(){
         $database = DatabaseFactory::getFactory()->getConnection();
+
+        
 
         $query = $database->prepare("SELECT * FROM hang_khach_hang rk WHERE 1  ORDER BY rk.muc_diem ASC");
         $query->execute();
